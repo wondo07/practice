ce1ab7b3440ad0e8d0c8cbbcab6afe22a7a01f6f
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-17 00:41:28 +0900

    commit

---
+++ src/main/java/com/example/practice/TodoApp.java
@@ -0,0 +1,14 @@
+package com.example.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoApp {
+
+    @GetMapping("/")
+    public String helloworld(){
+        return "To-do Application !";
+    }
+
+}
